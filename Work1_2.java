6e92df63977fa5294e4f3bb6c781525e059467f9
Владимир Ларин <90915991+vladimir2312@users.noreply.github.com>
2022-02-28 16:02:08 +0300

    Первое задание

--- Work1_2.java
+++ Work1_2.java
@@ -4,11 +4,11 @@ public class Work1_2 {
 	{
 	if(args.length > 0)
 	{
-		System.out.println("Вы ввели "+args.length+" параметров");
+		System.out.println("Вы ввели"+args.length+" параметров");
 	}
 	else
 	{
-		System.out.println("Вы не передавали параметров ");
+		System.out.println("Вы  не передавали параметров ");
 	}
 
 	}}
