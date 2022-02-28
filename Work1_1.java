071d5b0ebbabd27e76efb1f27a1b362750a715f8
Владимир Ларин <90915991+vladimir2312@users.noreply.github.com>
2022-02-28 16:01:28 +0300

    Первое задание

--- Work1_1.java
+++ Work1_1.java
@@ -2,7 +2,7 @@
 public class Work1_1 {
 	public static void main(String[] args)
 	{
-	System.out.println("Hello " + (args.length > 0? args[0] : "world"));
+	System.out.println("Hello" + (args.length > 0? args[0] : "world"));
 	}
 	}
 
