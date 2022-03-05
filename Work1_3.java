6ed03132f6eb843de750660b19558220ecc2dc8f
Владимир Ларин <90915991+vladimir2312@users.noreply.github.com>
2022-03-05 18:36:37 +0300

    Третье задание

--- Work1_3.java
+++ Work1_3.java
@@ -7,8 +7,7 @@ public class Work1_3 {
 	int b=Integer.parseInt(args[1]);
 	if(args.length == 2)
 	{
-		System.out.println(args[0]+"\n"+args[1]);
-		System.out.println(a+b);
+		System.out.println(args[0]+"+"+args[1]+ "=" +(a+b));
 	}
 	else
 	{
