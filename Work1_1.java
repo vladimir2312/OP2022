8e5f18163b0d7e2a3e73251adc6c156d6b8892b9
Владимир Ларин <90915991+vladimir2312@users.noreply.github.com>
2022-04-04 21:51:37 +0300

    Update Work1_1.java

--- Work1_1.java
+++ Work1_1.java
@@ -1,9 +1,7 @@
-
 public class Work1_1 {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args){
 	System.out.println("Hello " + (args.length > 0? args[0] : "world"));
 	}
-	}
+}
 
 
